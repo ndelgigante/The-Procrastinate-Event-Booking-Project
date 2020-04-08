186933b17a1e266409f4e2d5d6f2326c71f1a400
Austin Tindal <49575558+atindal@users.noreply.github.com>
2020-04-07 21:38:12 -0400

    add time

--- src/showing/Showing.java
+++ src/showing/Showing.java
@@ -1,37 +1,58 @@
 package showing;
-import events.Event; import events.Review; import venue.Room; import venue.*;
+import events.Event; import events.Review; import venue.*;
+import java.time.LocalDateTime;
 
 public class Showing {
 	private Event event;
 	private Room room; 
 	private Venue parentVenue;
+	private LocalDateTime time;
 	
-	public Showing(Event event, Venue parentVenue, int roomIdentifier) {
+	public Showing(Event event, Venue parentVenue, int roomIdentifier, LocalDateTime time) {
 		setEvent(event);
 		setParentVenue(parentVenue);
 		initRoom(roomIdentifier);
+		setTime(time);
 	}
 	
 	public Event getEvent() {
 		return event;
 	}
+	
 	public void setEvent(Event event) {
 		this.event = event;
 	}
-	public Room getRoom() {
-		return room;
-	}
+	
 	public Venue getParentVenue() {
 		return parentVenue;
 	}
+	
 	public void setParentVenue(Venue parentVenue) {
 		this.parentVenue = parentVenue;
 	}
 	
+	public LocalDateTime getTime() {
+		return this.time;
+	}
+	
+	public void setTime(LocalDateTime time) {
+		this.time = time;
+	}
+	
+	public Room getRoom() {
+		return room;
+	}
+	
+	/**
+	 * @param integer representing room template's position in parent venue array
+	 */
 	private void initRoom(int roomIdentifier) {
 		RoomTemplate roomTemplate = parentVenue.getRoom(roomIdentifier);
 		room = new Room(roomTemplate);
 	}
 	
+	public void print() {
+		System.out.println("Event: " + event.getName() + "Venue: "+parentVenue.getName() + "Time: "+time.toString());
+	}
 }
 
